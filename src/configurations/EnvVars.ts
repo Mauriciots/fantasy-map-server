13a6952173d694b927c460f0704b69259d6e07a8
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-10-24 21:54:38 -0700

    Sequelize basic setup

--- src/configurations/EnvVars.ts
+++ src/configurations/EnvVars.ts
@@ -1,22 +1,23 @@
 /* eslint-disable node/no-process-env */
 
 export default {
-  nodeEnv: (process.env.NODE_ENV ?? ''),
-  port: (process.env.PORT ?? 0),
+  nodeEnv: process.env.NODE_ENV ?? '',
+  port: process.env.PORT ?? 0,
   cookieProps: {
     key: 'ExpressGeneratorTs',
-    secret: (process.env.COOKIE_SECRET ?? ''),
+    secret: process.env.COOKIE_SECRET ?? '',
     options: {
       httpOnly: true,
       signed: true,
-      path: (process.env.COOKIE_PATH ?? ''),
+      path: process.env.COOKIE_PATH ?? '',
       maxAge: Number(process.env.COOKIE_EXP ?? 0),
-      domain: (process.env.COOKIE_DOMAIN ?? ''),
-      secure: (process.env.SECURE_COOKIE === 'true'),
+      domain: process.env.COOKIE_DOMAIN ?? '',
+      secure: process.env.SECURE_COOKIE === 'true',
     },
   },
   jwt: {
-    secret: (process.env.JWT_SECRET ??  ''),
-    exp: (process.env.COOKIE_EXP ?? ''), // exp at the same time as the cookie
+    secret: process.env.JWT_SECRET ?? '',
+    exp: process.env.COOKIE_EXP ?? '', // exp at the same time as the cookie
   },
+  postgresConnectionString: process.env.POSTGRES_CONNECTION_STRING ?? '',
 } as const;
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ import logger from 'jet-logger';
 import EnvVars from '@configurations/EnvVars';
 import server from './server';
 
+import { Sequelize } from 'sequelize';
 
 // **** Start server **** //
 
-const msg = ('Express server started on port: ' + EnvVars.port.toString());
-server.listen(EnvVars.port, () => logger.info(msg));
+const msg = 'Express server started on port: ' + EnvVars.port.toString();
+const sequelize = new Sequelize(EnvVars.postgresConnectionString);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info('Connection has been established successfully.');
+    server.listen(EnvVars.port, () => logger.info(msg));
+  } catch (error) {
+    logger.err(error);
+  }
+})();

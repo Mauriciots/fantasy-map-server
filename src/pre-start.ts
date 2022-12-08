4e540bbc5338dd7a60d4953adf5d1825936077f2
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-12-08 15:32:32 -0800

    Configure server startup & add command to Procfile

--- src/pre-start.ts
+++ src/pre-start.ts
@@ -1,6 +1,6 @@
 /**
- * Pre-start is where we want to place things that must run BEFORE the express 
- * server is started. This is useful for environment variables, command-line 
+ * Pre-start is where we want to place things that must run BEFORE the express
+ * server is started. This is useful for environment variables, command-line
  * arguments, and cron-jobs.
  */
 
@@ -10,7 +10,6 @@ import commandLineArgs from 'command-line-args';
 
 // **NOTE** Do not import any local paths here, or any libraries dependent
 // on environment variables.
-
 
 // **** Setup command line options **** //
 
@@ -23,13 +22,14 @@ const options = commandLineArgs([
   },
 ]);
 
-
 // **** Set the env file **** //
 
-const result2 = dotenv.config({
-  path: path.join(__dirname, `../env/${String(options.env)}.env`),
-});
+if (options.env !== 'production') {
+  const result2 = dotenv.config({
+    path: path.join(__dirname, `../env/${String(options.env)}.env`),
+  });
 
-if (result2.error) {
-  throw result2.error;
+  if (result2.error) {
+    throw result2.error;
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -13,16 +13,14 @@ import HttpStatusCodes from '@configurations/HttpStatusCodes';
 import { NodeEnvs } from '@declarations/enums';
 import { RouteError } from '@declarations/classes';
 
-
 // **** Init express **** //
 
 const app = express();
 
-
 // **** Set basic express settings **** //
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(EnvVars.cookieProps.secret));
 
 // Show routes called in console during development
@@ -35,54 +33,38 @@ if (EnvVars.nodeEnv === NodeEnvs.Production) {
   app.use(helmet());
 }
 
-
 // **** Add API routes **** //
 
 // Add APIs
 app.use('/api', BaseRouter);
 
 // Setup error handler
-app.use((
-  err: Error,
-  _: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction,
-) => {
-  logger.err(err, true);
-  let status = HttpStatusCodes.BAD_REQUEST;
-  if (err instanceof RouteError) {
-    status = err.status;
+app.use(
+  (
+    err: Error,
+    _: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
+  ) => {
+    logger.err(err, true);
+    let status = HttpStatusCodes.BAD_REQUEST;
+    if (err instanceof RouteError) {
+      status = err.status;
+    }
+    return res.status(status).json({ error: err.message });
   }
-  return res.status(status).json({ error: err.message });
-});
-
+);
 
 // **** Serve front-end content **** //
 
 // Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
+// const viewsDir = path.join(__dirname, 'views');
+// app.set('views', viewsDir);
 
 // Set static directory (js and css).
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
-
-// Nav to login pg by default
-app.get('/', (_: Request, res: Response) => {
-  res.sendFile('login.html', {root: viewsDir});
-});
-
-// Redirect to login if not logged in.
-app.get('/users', (req: Request, res: Response) => {
-  const jwt = req.signedCookies[EnvVars.cookieProps.key];
-  if (!jwt) {
-    res.redirect('/');
-  } else {
-    res.sendFile('users.html', {root: viewsDir});
-  }
-});
-
 
 // **** Export default **** //
 

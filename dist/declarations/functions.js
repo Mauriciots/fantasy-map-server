c873ed87b4c7263dda5581f23d251985aa951b76
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-12-08 17:52:50 -0800

    Remove dist folder

--- dist/declarations/functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomInt = void 0;
-function getRandomInt() {
-    return Math.floor(Math.random() * 1000000000000);
-}
-exports.getRandomInt = getRandomInt;

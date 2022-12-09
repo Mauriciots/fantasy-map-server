e508f003701a76aff17253324cab19a3744131ce
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-12-08 16:50:53 -0800

    Remove dist folder

--- dist/util/time.util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.timeFields = void 0;
-const timeFields = () => ({
-    createdAt: new Date(),
-    udpatedAt: new Date(),
-});
-exports.timeFields = timeFields;
